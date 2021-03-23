c3ba249ee8063c0272c64187ec25ab76ed832969
Gabriela Zalta <gzalta96@gmail.com>
2021-03-23 10:08:56 -0400

    project section done

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -23,9 +23,9 @@ function projects() {
             <div className="card m-4 project shadow-sm" style= {{ width: "100%" }}>
                 <img src={covid} className="card-img-top rounded" alt="COVID"/>
                 <div className="card-body">
-                    <p className="card-text"><a className="card-text" href="#">{"https://gabrielazalta.github.io/COVID-19-Live-Stats-Support/"}COVID-19 Live Stats and Support</a>
+                    <p className="card-text"><a className="card-text" href="https://gabrielazalta.github.io/COVID-19-Live-Stats-Support/">COVID-19 Live Stats and Support</a>
                     </p>
-                    <p className="card-text repo"><a className="card-text" href="#">{"https://github.com/gabrielazalta/COVID-19-Live-Stats-Support"}View Code</a>
+                    <p className="card-text repo"><a className="card-text" href="https://github.com/gabrielazalta/COVID-19-Live-Stats-Support">View Code</a>
                     </p>
                 </div>
             </div>
@@ -33,9 +33,9 @@ function projects() {
             <div className="card m-4 project shadow-sm" style= {{ width: "100%" }}>
                 <img src={schedule} className="card-img-top rounded" alt="scheduler"/>
                 <div className="card-body">
-                    <p className="card-text"><a className="card-text" href="#">{"https://gabrielazalta.github.io/work-day-scheduler/"}Work Day Scheduler</a>
+                    <p className="card-text"><a className="card-text" href="https://gabrielazalta.github.io/work-day-scheduler/">Work Day Scheduler</a>
                     </p>
-                    <p className="card-text repo"><a className="card-text" href="#">{"https://github.com/gabrielazalta/work-day-scheduler"}View Code</a>
+                    <p className="card-text repo"><a className="card-text" href="https://github.com/gabrielazalta/work-day-scheduler">View Code</a>
                     </p>
                 </div>
             </div>
@@ -43,9 +43,9 @@ function projects() {
             <div className="card m-4 project shadow-sm" style= {{ width: "100%" }}>
                 <img src={buddy} className="card-img-top rounded" alt="buddy"/>
                 <div className="card-body">
-                    <p className="card-text"><a className="card-text" href="#">{"https://gabrielazalta.github.io/run-buddy/"}Run Buddy</a>
+                    <p className="card-text"><a className="card-text" href="https://gabrielazalta.github.io/run-buddy/">Run Buddy</a>
                     </p>
-                    <p className="card-text repo"><a className="card-text" href="#">{"https://github.com/gabrielazalta/run-buddy"}View Code</a>
+                    <p className="card-text repo"><a className="card-text" href="https://github.com/gabrielazalta/run-buddy">View Code</a>
                     </p>
                 </div>
             </div>
@@ -56,9 +56,9 @@ function projects() {
             <div className="card m-4 project shadow-sm" style= {{ width: "100%" }}>
                 <img src={beer} className="card-img-top rounded" alt="draftRoom"/>
                 <div className="card-body">
-                    <p className="card-text"><a className="card-text" href="#">{"https://sleepy-island-61088.herokuapp.com/"}DraftRoom Beer Blog</a>
+                    <p className="card-text"><a className="card-text" href="https://sleepy-island-61088.herokuapp.com/">DraftRoom Beer Blog</a>
                     </p>
-                    <p className="card-text repo"><a className="card-text" href="#">{"https://github.com/gabrielazalta/draft-room/tree/main"}View Code</a>
+                    <p className="card-text repo"><a className="card-text" href="https://github.com/gabrielazalta/draft-room/tree/main">View Code</a>
                     </p>
                 </div>
             </div>
@@ -66,9 +66,9 @@ function projects() {
             <div className="card m-4 project shadow-sm" style= {{ width: "100%" }}>
                 <img src={weather} className="card-img-top rounded" alt="weather"/>
                 <div className="card-body">
-                    <p className="card-text"><a className="card-text" href="#">{"https://gabrielazalta.github.io/weather-dashboard/"}Weather Dashboard</a>
+                    <p className="card-text"><a className="card-text" href="https://gabrielazalta.github.io/weather-dashboard/">Weather Dashboard</a>
                     </p>
-                    <p className="card-text repo"><a className="card-text" href="#">{"https://github.com/gabrielazalta/weather-dashboard"}View Code</a>
+                    <p className="card-text repo"><a className="card-text" href="https://github.com/gabrielazalta/weather-dashboard">View Code</a>
                     </p>
                 </div>
             </div>
@@ -76,9 +76,9 @@ function projects() {
             <div className="card m-4 project shadow-sm" style= {{ width: "100%" }}>
                 <img src={git} className="card-img-top rounded" alt="git"/>
                 <div className="card-body">
-                    <p className="card-text"><a className="card-text" href="#">{"https://gabrielazalta.github.io/git-it-done/"}Git It Done</a>
+                    <p className="card-text"><a className="card-text" href="https://gabrielazalta.github.io/git-it-done/">Git It Done</a>
                     </p>
-                    <p className="card-text repo"><a className="card-text" href="#">{"https://github.com/gabrielazalta/git-it-done"}View Code</a>
+                    <p className="card-text repo"><a className="card-text" href="https://github.com/gabrielazalta/git-it-done">View Code</a>
                     </p>
                 </div>
             </div>
