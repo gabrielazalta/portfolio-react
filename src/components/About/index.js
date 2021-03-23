ab253a84268e6a3bde360256ff7c160e872fe3b1
Gabriela Zalta <gzalta96@gmail.com>
2021-03-23 12:39:02 -0400

    readme updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -24,6 +24,12 @@ function About() {
               Hello! My name is Gabriela Zalta, I'm from Bogota, Colombia and I am a dynamic full-stack web developer currently seeking opportunities in front-end development.  I graduated from the University of Central Florida in 2019 with a degree in Integrated Business and a minor in International Business. After working in the business industry for a while, I realized that I wanted to shift gears a little and pursue my dream of becoming a programmer! As I learn more about web development and design, I have become increasingly excited about reaching my goals and to see what my future holds.
             </p>
           </div>
+          <p className="m-4 p-4 col intro-text">
+            Languages: HTML, CSS, JavaScript, SQL, NoSQL
+          </p>
+          <p className="m-4 p-4 col intro-text">
+            Tools: MERN, Handlebars, Bootstrap, Bulma, JQuery
+          </p>
         </div>
         <div className="col-1"></div>
       </div>
