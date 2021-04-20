440db42c4fff0317ae4d74c9d613d56085c1f817
Gabriela Zalta <gzalta96@gmail.com>
2021-04-20 15:06:33 -0500

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import resume from "../../assets/images/resume.JPG";
-import downloadResume from "../../assets/pdf/downloadResume.pdf";
+import resume from "../../assets/pdf/resume.pdf";
 
 function Resume() {
     return (
@@ -21,7 +21,7 @@ function Resume() {
                         <div className="card m-4 project shadow-sm" style= {{ width: "100%", backgroundColor: "#fbefcc9a" }}>
                             <img src={resume} className="card-img-top rounded" alt="resume"/>
                             <div className="card-body">
-                                <p className="card-text"><a className="card-text" href={downloadResume} to="route" target="_blank" rel="noopener noreferrer">download resume</a>
+                                <p className="card-text"><a className="card-text" href={resume} to="route" target="_blank" rel="noopener noreferrer">download resume</a>
                                 </p>
                             </div>
                         </div>
